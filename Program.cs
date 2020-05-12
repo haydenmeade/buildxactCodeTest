c041963cbc97ccbefbd09bd48c6776d83eecaf0f
haydenmeade <qwertyda2nd@gmail.com>
2020-05-12 19:15:36 +1000

    Add conversion and console output

--- Program.cs
+++ Program.cs
@@ -28,17 +28,17 @@ namespace SuppliesPriceLister
             combinedSupplies.AddRange(new JsonSupplyReader().ReadSupplies("megacorp.json", Currency.FromCode("USD")));
 
             // AUD to USD exchanger.
-            var exch = new ExchangeRate("AUD", "USD", audToUsd);
+            var aud = Currency.FromCode("AUD");
+            var usd = Currency.FromCode("USD");
+            var exch = new ExchangeRate(aud, usd, audToUsd);
 
-            // Convert all the prices to AUD.
-            // TODO test this only converts USD not AUD
-            combinedSupplies.ForEach(s => s.Price = exch.Convert(s.Price));
+            // Convert all the prices to AUD if needed.
+            combinedSupplies.ForEach(s => s.Price = s.Price.Currency == aud ? s.Price : exch.Convert(s.Price));
 
             // Output the supplies in price order.
             foreach (ISupply s in combinedSupplies.OrderByDescending(s => s.Price))
             {
-                // TODO correct output.
-                Console.WriteLine("...");
+                Console.WriteLine(s.ToString());
                 Console.WriteLine();
             }
         }
