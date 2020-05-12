09c27a7ef41275b2863f0408ea72f1490678aa6c
haydenmeade <qwertyda2nd@gmail.com>
2020-05-12 18:14:06 +1000

    Configuration setup. And Program outline

--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using buildxact_supplies.Models;
+using Microsoft.Extensions.Configuration;
+using NodaMoney;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace SuppliesPriceLister
 {
@@ -6,13 +12,34 @@ namespace SuppliesPriceLister
     {
         static void Main(string[] args)
         {
+
+            // read the configuration file for the exchange rate.
+            var builder = new ConfigurationBuilder()
+                  .SetBasePath(Directory.GetCurrentDirectory())
+                  .AddJsonFile("appsettings.json");
+
+            var configuration = builder.Build();
+            var audToUsd = double.Parse(configuration["audUsdExchangeRate"]);
+
             // Read the data.
-            var csvReader = new CsvSupplyReader();
-            var jsonReader = new JsonSupplyReader();
+            var combinedSupplies = new List<ISupply>();
+            combinedSupplies.AddRange(new CsvSupplyReader().ReadSupplies("humphries.csv", Currency.FromCode("AUD")));
+            combinedSupplies.AddRange(new JsonSupplyReader().ReadSupplies("megacorp.json", Currency.FromCode("USD")));
 
-            // Combine the supplies.
+            // AUD to USD exchanger.
+            var exch = new ExchangeRate("AUD", "USD", audToUsd);
 
-            // Convert to AUD -> Sort -> Print.
+            // Convert all the prices to AUD.
+            // TODO test this only converts USD not AUD
+            combinedSupplies.ForEach(s => s.Price = exch.Convert(s.Price));
+
+            // Output the supplies in price order.
+            foreach (ISupply s in combinedSupplies.OrderByDescending(s => s.Price))
+            {
+                // TODO correct output.
+                Console.WriteLine("...");
+                Console.WriteLine();
+            }
         }
     }
 }
